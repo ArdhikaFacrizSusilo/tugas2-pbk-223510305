f29c7f5420838c57860332c23fd05b9de702eae7
ArdhikaFacrizSusilo <ardhika.facriz@gmail.com>
2024-05-02 18:24:20 +0700

    tugas 2 pbk

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      greeting: 'Coba ketik dong!',
+      inputText: '',
+      isRed: false,
+      isBold: false
+    };
+  },
+  computed: {
+    dynamicText() {
+      return this.inputText !== '' ? 'Input: ' + this.inputText : 'Isi input untuk melihat perubahan tulisannya';
+    }
+  },
+  methods: {
+    updateGreeting() {
+      this.greeting = 'Coba ketik dong, ' + this.inputText + '!';
+      this.isRed = this.inputText.length > 5; // Jika panjang teks lebih dari 5 karakter, ubah warna menjadi biru
+      this.isBold = this.inputText.length % 2 === 0; // Jika panjang teks genap, buat teks menjadi tebal
+    }
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <h1>{{ greeting }}</h1>
+    
+    <input type="text" v-model="inputText" @input="updateGreeting">
+    
+    <div :class="{ 'red-text': isRed }" :style="{ 'font-weight': isBold ? 'bold' : 'normal' }">
+        {{ dynamicText }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+
+.red-text {
+  color: rgb(35, 0, 163);
+}
+</style>
